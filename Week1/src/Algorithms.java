a042a17eef40774cc8af294b7febd39550fc541a
Talmacel Larisa <larisa-maria.talmacel@student.tuiasi.ro>
2025-03-05 15:27:03 +0200

    added the problems' solutuins

--- Week1/src/Algorithms.java
+++ Week1/src/Algorithms.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.*;
+
 public class Algorithms {
     public static List<Double> GetMultiples(double num, int length){
         List<Double> results = new ArrayList<>();
@@ -10,9 +12,61 @@ public class Algorithms {
         }
 
         for (double e: results){
-            System.out.print(e +" ");
+            System.out.println(e +" ");
         }
 
         return results;
+    }
+
+    public static double[] otherSides(int n) {
+        // n, n*2=hypotenuse, (n*2*rad(3))/2 = medium length side
+        double hypotenuse = 2*n;
+        double mediumLengthSide = n * sqrt(3);
+
+        String result = String.format("smallerLengthSide: %d, Hypotenuse: %.2f and mediumLengthSide: %.2f%n", n, hypotenuse,mediumLengthSide);
+        System.out.print(result);
+
+        return new double[]{hypotenuse, mediumLengthSide};
+    }
+
+    public static int warOfNumbers(int[]numbers){
+        int even=0, odd=0;
+        for (int number:numbers){
+            if(number%2 == 0){
+                even+=number;
+            }
+            else{
+                odd+=number;
+            }
+        }
+        System.out.println("War of numbers result: " + abs(even-odd));
+        return abs(even-odd);
+    }
+
+    public static int letterCounter(char[][] arr, char c) {
+        int counter = 0;
+        for (char[] innerList: arr){
+            for (char letter: innerList){
+                if (letter == c){
+                    counter++;
+                }
+            }
+        }
+        System.out.println("Letter " + c + " occurs "+ counter + " times.");
+        return counter;
+    }
+
+    public static boolean isFactorial(int n, int current, int factorial) {
+        if (n == factorial){
+            System.out.println(n + " is exactly the factorial of " + current);
+            return true;
+        }
+
+        if (factorial > n){
+            System.out.println("no factorial");
+            return false;
+        }
+
+        return isFactorial(n, current+1, factorial * (current+1));
     }
 }
--- Week1/src/Main.java
+++ Week1/src/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
         int i = 5;
         for (; i > 0; i--) {
-            System.out.println("Ddd");
+            System.out.println("ok");
         }
 
         int j = 5;
         while (j >= 0) {
-            System.out.println("eee");
+            System.out.println("ok");
             j--;
         }
         System.out.println(j);
@@ -29,11 +29,35 @@ public class Main {
 
         ///// problems
         //1
+        System.out.println("-----------------------------1 ----------------------");
         List<Double> multiples = Algorithms.GetMultiples(12,10);
 
         //2
+        System.out.println("-----------------------------2 ----------------------");
+        Algorithms.otherSides(1);
+        Algorithms.otherSides(12);
+        Algorithms.otherSides(2);
+
         //3
+        System.out.println("-----------------------------3 ----------------------");
+        Algorithms.warOfNumbers(new int []{2,8,7,5});
+        Algorithms.warOfNumbers(new int []{12, 90, 75});
+        Algorithms.warOfNumbers(new int []{5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243});
+
         //4
+        System.out.println("-----------------------------4 ----------------------");
+        char[][] letterMatrix = {
+                {'D', 'E', 'Y', 'H', 'A', 'D'},
+                {'C', 'B', 'Z', 'Y', 'J', 'K'},
+                {'D', 'B', 'C', 'A', 'M', 'N'},
+                {'F', 'G', 'G', 'R', 'S', 'R'},
+                {'V', 'X', 'H', 'A', 'S', 'S'}
+        };
+        Algorithms.letterCounter(letterMatrix, 'D') ;
+
         //5
+        System.out.println("-----------------------------5 ----------------------");
+        Algorithms.isFactorial(24,1,1); //mereu 1 si 1 pt ultimele 2 args
+        Algorithms.isFactorial(120,1,1);
     }
 }
