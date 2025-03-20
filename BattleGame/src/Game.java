440f70b7e519ccedb96860bae1dd8e1f8cf76826
Talmacel Larisa <larisa-maria.talmacel@student.tuiasi.ro>
2025-03-20 12:53:26 +0200

    refactor attack functionality

--- BattleGame/src/Game.java
+++ BattleGame/src/Game.java
@@ -9,46 +9,42 @@ public class Game {
         this.player2 = p2;
     }
 
-    public void startRound() {
+    public void attack(Player attacker, Player enemy, int chanceToHit) {
         int currentLifeScore;
+        System.out.println("Current atacker: " + attacker.getUserName());
+        attacker.getWeapon().useWeapon();
+        System.out.println("The actual chance to hit with attacker's weapon is " + attacker.getWeapon().getProbability());
+        if (chanceToHit <= attacker.getWeapon().getProbability()) {
+            currentLifeScore = enemy.getLifeScore() - attacker.getWeapon().getDamage();
+            System.out.println("You struck him straight in the heart! The remaining life score for " + enemy.getUserName() + " is: " + enemy.getLifeScore() + "-" + attacker.getWeapon().getDamage() + " =" + currentLifeScore);
+
+            //if the value is less than 0, then set it to 0
+            enemy.setLifeScore(Math.max(0, currentLifeScore));
+        } else {
+            System.out.println("You missed..maybe next time you'll hit the opponent");
+        }
+    }
+
+    public void startRound() {
         while (player1.getLifeScore() > 0 && player2.getLifeScore() > 0) {
             System.out.println('\n' + "------------- next atack ------------- ");
-            System.out.println("Life Score for player1: " + player1.getLifeScore());
-            System.out.println("Life Score for player2: " + player2.getLifeScore());
+            System.out.println("Life Score for " + player1.getUserName() + ": " + player1.getLifeScore());
+            System.out.println("Life Score for " + player2.getUserName() + ": " + player2.getLifeScore());
 
             Random random = new Random();
 
-            //sa generez cumva random cine pe cine loveste
+            // generate which player attacks
             int atacker = random.nextInt(2) + 1;
-            System.out.println("Current Atacker: player" + atacker);
-            //sa generez random un nr de la 1 la 100 pt fiecare lovitura; daca e <= sansa atunci l-a nimerit, altfel nu
+            // generate a random number from 1 to 100 for each hit; if it's <= probability, then he hit, otherwise not
             int randomChance = random.nextInt(100) + 1;
             System.out.println("(RANDOM) Chance to hit the enemy: " + randomChance);
             if (atacker == 1) {
-                player1.getWeapon().useWeapon();
-                System.out.println("The actual chance to hit with player1's weapon is " + player1.getWeapon().getProbability());
-                if (randomChance <= player1.getWeapon().getProbability()) {
-                    currentLifeScore = player2.getLifeScore() - player1.getWeapon().getDamage();
-                    System.out.println("You struck him straight in the heart! The remaining life score for player2 is: " + player2.getLifeScore() + "-" + player1.getWeapon().getDamage() + " =" + currentLifeScore);
-
-                    //if the value is less than 0, then set it to 0
-                    player2.setLifeScore(Math.max(0, currentLifeScore));
-                } else {
-                    System.out.println("You missed..maybe next time you'll hit the opponent");
-                }
+                attack(player1, player2, randomChance);
             } else {
-                player2.getWeapon().useWeapon();
-                System.out.println("The actual chance to hit with player2's weapon is " + player2.getWeapon().getProbability());
-                if (randomChance <= player2.getWeapon().getProbability()) {
-                    currentLifeScore = player1.getLifeScore() - player2.getWeapon().getDamage();
-                    System.out.println("You struck him straight in the heart! The remaining life score for player1 is: " + player1.getLifeScore() + "-" + player2.getWeapon().getDamage() + " =" + currentLifeScore);
-
-                    player1.setLifeScore(Math.max(0, currentLifeScore));
-                } else {
-                    System.out.println("You missed..maybe next time you'll hit the opponent");
-                }
+                attack(player2, player1, randomChance);
             }
         }
+
         System.out.println('\n' + "GAME OVER!");
         System.out.println("Final score for " + player1.getUserName() + ": " + player1.getLifeScore());
         System.out.println("Final score for " + player2.getUserName() + ": " + player2.getLifeScore());
