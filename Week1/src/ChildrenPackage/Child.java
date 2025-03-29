eaafed8d07708020327d71b38c7cc7431b31dcb7
Talmacel Larisa <larisa-maria.talmacel@student.tuiasi.ro>
2025-03-29 09:23:46 +0200

    added some classes

---
+++ Week1/src/ChildrenPackage/Child.java
@@ -0,0 +1,10 @@
+package ChildrenPackage;
+
+import ParentsPackage.Parent;
+
+public class Child extends Parent {
+    public void test(){
+        Parent parent  = new Parent();
+        //System.out.println(parent.age);
+    }
+}
---
+++ Week1/src/ParentsPackage/Parent.java
@@ -0,0 +1,5 @@
+package ParentsPackage;
+
+public class Parent {
+    protected int age;
+}
